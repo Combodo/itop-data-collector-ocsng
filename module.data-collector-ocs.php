7fb838156021d39df427790e483713daec69ba71
Denis Flaven <denis.flaven@combodo.com>
2018-09-04 17:07:17 +0200

    Preparing for the first public version: added some parameters to the data sources and use tickets_list to store the OCSng ID in order NOT to require any alteration of the data model for the extension showing the extra tab OCS to work.

--- module.data-collector-ocs.php
+++ module.data-collector-ocs.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'data-collector-ocs/1.0.0',
+	'data-collector-ocs/1.0.1',
 	array(
 		// Identification
 		//
