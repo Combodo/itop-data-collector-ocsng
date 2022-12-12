16f7c8d8ee6722b6204b75d2dddaa3902f763312
acognet <anne-catherine.cognet@combodo.com>
2022-12-12 14:54:06 +0100

    PHP 8.0 & 8.1 compatibility

--- src/AbstractOCSSoftwareCollector.php
+++ src/AbstractOCSSoftwareCollector.php
@@ -18,10 +18,14 @@ abstract class AbstractOCSSoftwareCollector extends AbstractOCSCollector
     protected function AddOtherParams(&$sQuery)
     {
         if (Utils::GetConfigurationValue("use_software_categories", 'no') == 'yes') {
-            $sQueryITop = "SELECT  OCSAssetSoftware WHERE type='" . $this->GetTargetClass() . "'";
+            $sQueryITop = "SELECT  OCSSoftwareCategorie WHERE type='" . $this->GetTargetClass() . "'";
             $oRestClient = new RestClient();
-            $aResult = $oRestClient->Get("OCSAssetSoftware", $sQueryITop, "name");
-
+            $aResult = $oRestClient->Get("OCSSoftwareCategorie", $sQueryITop, "name, type");
+            if(is_null($aResult['objects']))
+            {
+                Utils::Log(LOG_ERR, "No OCSSoftwareCategorie found in iTop.");
+                return;
+            }
             $aListCategories = [];
             foreach ($aResult['objects'] as $idx => $aAttDef) {
                 $aListCategories[$aAttDef['fields']['name']] = $aAttDef['fields']['name'];
@@ -29,5 +33,33 @@ abstract class AbstractOCSSoftwareCollector extends AbstractOCSCollector
 
             $sQuery = str_replace('#ERROR_UNDEFINED_PLACEHOLDER_categorielist#', implode("','", $aListCategories), $sQuery);
         }
+        else {
+            $sQueryITop = Utils::GetConfigurationValue("OCSSoftware_getListFromItop", '');
+            $oRestClient = new RestClient();
+            $aResult = $oRestClient->Get("Software", $sQueryITop, "name, type");
+
+            $aListSoftware = [];
+            foreach ($aResult['objects'] as $idx => $aAttDef) {
+                $sType = $aAttDef['fields']['type'];
+                if (is_null($sType)) {
+                    $sType = 'OtherSoftware';
+                }
+                $aListSoftware[$sType][$aAttDef['fields']['name']] = $aAttDef['fields']['name'];
+            }
+        }
+
+        $sInitialQuery = $sQuery;
+        $bIsFirst = true;
+        foreach ($aListSoftware as $sType => $aName) {
+            $sQueryByType = str_replace('#ERROR_UNDEFINED_PLACEHOLDER_softwarelist#', implode("','", $aName), $sInitialQuery);
+            $sQueryByType = str_replace('#ERROR_UNDEFINED_PLACEHOLDER_type_id#', $sType, $sQueryByType);
+            if ($bIsFirst) {
+                $sQuery = $sQueryByType;
+                $bIsFirst = false;
+            } else {
+                $sQuery = $sQuery . ' UNION ' . $sQueryByType;
+            }
+        }
+
     }
 }
